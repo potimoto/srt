a699c703aff01ea67584eea604d0a1ac914a1f3b
Kouhei Takimoto <43897696+potimoto@users.noreply.github.com>
2021-08-17 16:17:32 +0900

    fix code format

--- sample.srt.js
+++ sample.srt.js
@@ -1,16 +1,13 @@
 1
 00:00:00,000 --> 00:00:01,000
 alert("hello world");
-
 loadScript('https://static.robotwebtools.org/EventEmitter2/current/eventemitter2.min.js', function(e){
     console.log("robot web tools loaded");
 });
 loadScript('https://static.robotwebtools.org/roslibjs/current/roslib.js', function(e){
     console.log("roslib loaded");
 });
-
 ros = new ROSLIB.Ros();
-
 ros.on('error', function(error) {
     document.getElementById('connecting').style.display = 'none';
     document.getElementById('connected').style.display = 'none';
@@ -18,7 +15,6 @@ ros.on('error', function(error) {
     document.getElementById('error').style.display = 'inline';
     console.log(error);
   });
-
 ros.on('connection', function() {
     console.log('Connection made!');
     document.getElementById('connecting').style.display = 'none';
@@ -26,27 +22,22 @@ ros.on('connection', function() {
     document.getElementById('closed').style.display = 'none';
     document.getElementById('connected').style.display = 'inline';
 });
-
 ros.on('close', function() {
     console.log('Connection closed.');
     document.getElementById('connecting').style.display = 'none';
     document.getElementById('connected').style.display = 'none';
     document.getElementById('closed').style.display = 'inline';
 });
-
 //ros.connect('ws://192.168.10.122:9090');
 ros.conect('ws://000412d18d4c.ngrok.io');
-
 teleop = new ROSLIB.Topic({
     ros : ros,
     name : '/cmd_vel',
     messageType : 'geometry_msgs/Twist'
 });
-
 let idx;
 az = [1, 0, -1, 1, 0, -1, -1, 0, 1];
 lx = [1, 1, 1, 0, 0, 0, -1, -1, -1];
-
 function pub(idx) {
     var msg = new ROSLIB.Message({
              angular: {
@@ -64,7 +55,6 @@ function pub(idx) {
     teleop.publish(msg);
     console.log(msg);
 }
-
 pub(0);
 
 2
