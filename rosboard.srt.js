980e51f0d6750bc88ef0a87d9dd4924caec3fb29
potimoto <awakenthepower0101@gmail.com>
2022-01-04 15:26:05 +0900

    add example message

--- rosboard.srt.js
+++ rosboard.srt.js
@@ -3,7 +3,9 @@
 let turtleX = 3.0;
 let turtleZ = 2.0;
 let ws = new WebSocket('ws://192.168.1.13:8888/');
-ws.send("hello!");
+this.send(JSON.stringify([WebSocketV1Transport.SRT_MSG, {
+    ["x"]: this.turtleX,
+    ["y"]: this.turtleY}]));
 2
 00:00:03,000 --> 00:00:04,000
 let turtleX = 0.0;
