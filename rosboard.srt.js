a7668fbcaf46112d4c863f59cf061bbc4c8b4478
potimoto <awakenthepower0101@gmail.com>
2022-01-12 16:10:46 +0900

    Fix function call part

--- rosboard.srt.js
+++ rosboard.srt.js
@@ -1,13 +1,8 @@
 1
 00:00:00,000 --> 00:00:01,000
-function controller(){
-    const turtle = [3.0, 2.0];
-    return turtle;
-}
+const turtle = [3.0,2.0];
+controller(turtle);
 
 2
 00:00:05,000 --> 00:00:04,000
-function controller(){
-    const turtle = [0.0, 0.0];
-    return turtle;
-}
+const turtle = [0.0,0.0];
