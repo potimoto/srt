4e8681256146021b9f16547eac8ef68d5c09271e
potimoto <awakenthepower0101@gmail.com>
2022-01-13 15:33:13 +0900

    change teleop message

--- rosboard.srt.js
+++ rosboard.srt.js
@@ -1,9 +1,14 @@
 1
 00:00:00,000 --> 00:00:01,000
-const turtle = [3.0,2.0];
+const turtle = [0.0,1.0];
 controller(turtle);
 
 2
 00:00:05,000 --> 00:00:06,000
+const turtle = [0.0,-1.0];
+controller(turtle);
+
+3
+00:00:10,000 --> 00:00:11,000
 const turtle = [0.0,0.0];
 controller(turtle);
