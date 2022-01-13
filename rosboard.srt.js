d907416e550b9af718ec85b4ce26ca67eab3c8db
potimoto <awakenthepower0101@gmail.com>
2022-01-13 15:41:03 +0900

    change messaging value

--- rosboard.srt.js
+++ rosboard.srt.js
@@ -1,11 +1,11 @@
 1
 00:00:00,000 --> 00:00:01,000
-const turtle = [0.0,1.0];
+const turtle = [0.0,0.9];
 controller(turtle);
 
 2
 00:00:05,000 --> 00:00:06,000
-const turtle = [0.0,-1.0];
+const turtle = [0.0,-0.9];
 controller(turtle);
 
 3
