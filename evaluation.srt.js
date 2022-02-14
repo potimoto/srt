6602482180e52f883863b7bec9751aef3930a0c2
potimoto <awakenthepower0101@gmail.com>
2022-02-14 15:09:20 +0900

    Delete import

--- evaluation.srt.js
+++ evaluation.srt.js
@@ -1,6 +1,5 @@
 1
 00:00:01,000 --> 00:00:05,999
-import { Temporal } from "@js-temporal/polyfill";
 const instant = Temporal.Now.instant();
 console.log(instant.epochNanoseconds);
 const turtle = [0.000,0.09];
