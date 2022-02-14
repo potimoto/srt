1ad5c3d15566e19c0018156962d7eb17dd383388
potimoto <awakenthepower0101@gmail.com>
2022-02-14 14:41:55 +0900

    Change from date() object to temporal

--- evaluation.srt.js
+++ evaluation.srt.js
@@ -1,6 +1,7 @@
 1
 00:00:01,000 --> 00:00:05,999
-let date = new Date();
-console.log(date.getTime());
+import { Temporal } from "@js-temporal/polyfill";
+const instant = Temporal.Now.instant();
+console.log(instant.epochNanoseconds);
 const turtle = [0.000,0.09];
 controller(turtle);
