abecea68f6e41b2b4cf03007c3acaf49dab1c156
瀧本恒平 <kouheitakimoto@hanmayuujirounoMacBook-Pro.local>
2022-05-30 18:17:28 +0900

    add robomech.srt.js

--- evaluation.srt.js
+++ evaluation.srt.js
@@ -1,6 +1,6 @@
 1
 00:00:01,000 --> 00:00:05,999
-const instant = Temporal.Now.instant();
-console.log(instant.epochNanoseconds);
+let date = new Date();
+console.log(date.getTime());
 const turtle = [0.000,0.09];
 controller(turtle);
---
+++ robomech.srt.js
@@ -0,0 +1,9 @@
+1
+00:00:06,000 --> 00:00:15,999
+const turtle = [1.0,0.0];
+controller(turtle);
+
+2
+00:00:16,000 --> 00:00:17,000
+const turtle = [0.0,0.0];
+controller(turtle);
