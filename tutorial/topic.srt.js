b23a7d34b6341a450c8b92826d5e7ab0f1a50e2e
瀧本恒平 <kouheitakimoto@hanmayuujirounoMacBook-Pro.local>
2022-07-21 10:17:56 +0900

    予備実験用srtファイルの追加

---
+++ tutorial/topic.srt.js
@@ -0,0 +1,89 @@
+1
+00:04:12,000 --> 00:04:23,500
+const turtle = [0.01,0.00];
+controller(turtle);
+
+2
+00:04:23,501 --> 00:04:23,999
+const turtle = [0.00,0.00];
+controller(turtle);
+
+3
+00:06:37,400 --> 00:06:43,000
+const turtle = [0.00,1.00];
+controller(turtle);
+
+4
+00:06:43,001 --> 00:06:43,999
+const turtle = [0.00,0.00];
+controller(turtle);
+
+5
+00:07:15,500 --> 00:07:17,000
+const turtle = [0.00,1.00];
+controller(turtle);
+
+6
+00:07:17,001 --> 00:07:17,100
+const turtle = [0.00,0.00];
+controller(turtle);
+
+7
+00:07:17,101 --> 00:07:19,000
+const turtle = [0.00,1.00];
+controller(turtle);
+
+8
+00:07:19,001 --> 00:07:19,100
+const turtle = [0.00,0.00];
+controller(turtle);
+
+9
+00:07:19,101 --> 00:07:20,900
+const turtle = [0.00,1.00];
+controller(turtle);
+
+10
+00:07:20,901 --> 00:07:20,999
+const turtle = [0.00,0.00];
+controller(turtle);
+
+11
+00:07:21,000 --> 00:07:22,999
+const turtle = [0.00,1.00];
+controller(turtle);
+
+12
+00:07:23,000 --> 00:07:23,100
+const turtle = [0.00,0.00];
+controller(turtle);
+
+13
+00:07:23,101 --> 00:07:24,999
+const turtle = [0.00,1.00];
+controller(turtle);
+
+14
+00:07:25,000 --> 00:07:26,999
+const turtle = [0.00,0.00];
+controller(turtle);
+
+13
+00:07:27,000 --> 00:07:28,700
+const turtle = [0.00,1.00];
+controller(turtle);
+
+14
+00:07:28,700 --> 00:07:28,999
+const turtle = [0.00,0.00];
+controller(turtle);
+
+15
+00:07:29,000 --> 00:07:30,500
+const turtle = [0.00,1.00];
+controller(turtle);
+
+16
+00:07:30,501 --> 00:07:30,999
+const turtle = [0.00,0.00];
+controller(turtle);
