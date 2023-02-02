6a4617471502f0ac77ab9e51cf313c8a436116d3
瀧本恒平 <kouheitakimoto@hanmayuujirounoMacBook-Pro.local>
2023-02-02 16:49:15 +0900

    pubsubチュートリアル用srtの作成

---
+++ tutorial/pubsub.srt.js
@@ -0,0 +1,24 @@
+1
+00:10:58,000 --> 00:11:08,500
+const turtle = [0.05,0.3];
+controller(turtle);
+
+2
+00:11:09,000 --> 00:13:27,999
+const turtle = [0.00,0.00];
+controller(turtle);
+
+3
+00:13:28,000 --> 00:13:33,000
+const turtle = [0.1,0.00];
+controller(turtle);
+
+4
+00:13:33,001 --> 00:13:35,001
+const turtle = [0.00,0.785];
+controller(turtle);
+
+5
+00:13:35,002 --> 00:15:00,000
+const turtle = [0.00,0.00];
+controller(turtle);
